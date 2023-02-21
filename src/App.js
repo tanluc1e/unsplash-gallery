ca127099177a0b78e2c6b30e7436bc6a11d040b7
tanluc1e <t.luc497303@gmail.com>
2023-02-21 15:07:22 +0700

    feat: user profile

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import GetImages from "./GetImages";
+import Profile from "./Profile";
 
 export default function App() {
-  return <GetImages />;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<GetImages />}></Route>
+          <Route path="/user/:username" element={<Profile />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
--- src/GetImages.js
+++ src/GetImages.js
@@ -1,8 +1,10 @@
 import { useState, useEffect } from "react";
-import Image from "./Image";
+import Article from "./Article";
+// import { Link } from "react-router-dom"
 
 export default function GetImages() {
   const [images, setImages] = useState([]);
+
   useEffect(() => {
     const fetchImages = async () => {
       const response = await fetch(
@@ -18,23 +20,23 @@ export default function GetImages() {
 
   return (
     <>
-      {!images ? (
-        <h1 className="flex items-center justify-center h-screen font-bold text-4xl text-center text-slate-800">
-          Loading...
+      <div className="container mx-auto px-5 2xl:px-0">
+        <h1 className="text-slate-800 font-bold text-3xl md:text-4xl lg:text-6xl my-10 lg:mt-20 lg:mb-14">
+          Recommended For You
         </h1>
-      ) : (
-        <section className="px-5 container mx-auto">
-          <h1 className="font-bold text-3xl md:text-4xl lg:text-6xl my-10 lg:mt-20 lg:mb-16 text-slate-800 capitalize">
-            Recommended for you
-          </h1>
 
-          <div className="grid grid-cols-1 gap-10 md:grid-cols-2 lg:grid-cols-3">
-            {images.map((image, index) => (
-              <Image key={image.id} {...image} />
-            ))}
+        {!images ? (
+          <div>
+            <h1>Loading...</h1>
           </div>
-        </section>
-      )}
+        ) : (
+          <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-3 pb-20 lg:container">
+            {images.map((image) => (
+              <Article key={image.id} {...image} />
+            ))}
+          </section>
+        )}
+      </div>
     </>
   );
 }
